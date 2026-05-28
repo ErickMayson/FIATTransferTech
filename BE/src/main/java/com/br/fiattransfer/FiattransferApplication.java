1f056860d3173a11e659bc3aae205cc1aa36c5ee
Erick Ferreira <ferreiraerick0101@gmail.com>
2026-05-28 20:40:34 -0300

    FEAT: Create Java Project

---
+++ BE/src/main/java/com/br/fiattransfer/FiattransferApplication.java
@@ -0,0 +1,13 @@
+package com.br.fiattransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiattransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiattransferApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/com/br/fiattransfer/FiattransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.fiattransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiattransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
